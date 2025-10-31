af47e0249b0ae37e62ab28d298181f6aebf7e696
sjjleachexeter <sl1169@exeter.ac.uk>
2025-10-31 10:15:01 +0000

    Mulltiple.hs (test 1)(

--- coursework/Multiple.hs
+++ coursework/Multiple.hs
@@ -1,4 +1,4 @@
---### Question 1.1 Number ###--
+-- ### Question 1.1 Number ### --
 number :: [Int] -> Int -- using Int not Integer as explained in lectures
 number xs
   = totalize (reverse xs)
@@ -8,30 +8,37 @@ number xs
     totalize (x:xs)
       = x + 10 * totalize xs
 
---### Question 1.2 Generator ###--
+-- ### Question 1.2 Generator ### --
 generator1 :: [(Int,Int,Int,Int,Int,Int)]
 generator1 =
-    [(A,B,C,D,E,F)
+    [(a,b,c,d,e,f)
     -- not the most beautiful method maybe, but it excludes the need to check if they
     -- are non-zero by only drawing from 1-9
-    | A <- [1..9]
-    , B <- [1..9], B /= A 
-    , C <- [1..9], C /= B, C /= A 
-    , D <- [1..9], D /= C, D /= B, D /= A 
-    , E <- [1..9], E /= D, E /= C, E /= B, E /= A
-    , F <- [1..9], F /= E, F /= D, F /= C, F /= B, F /= A
+    | a <- [1..9]
+    , b <- [1..9], b /= a 
+    , c <- [1..9], c /= b, c /= a 
+    , d <- [1..9], d /= c, d /= b, d /= a 
+    , e <- [1..9], e /= d, e /= c, e /= b, e /= a
+    , f <- [1..9], f /= e, f /= d, f /= c, f /= b, f /= a
     ]
     
     
---### is perfect Sqr is a function to help the selector in 1.3 ###--
+-- ### is perfect Sqr is a function to help the selector in 1.3 ### --
 isperfSqr :: Int -> Bool
 isperfSqr n = 
-    sqrt n == sqrt (floor n)
+    p * q == n
+    where
+     p = floor (sqrt (fromIntegral n))
+     q = floor (sqrt (fromIntegral n))
 
 
---### Question 1.3 Selector ###--
+-- ### Question 1.3 Selector ### --
 selector1 :: (Int,Int,Int,Int,Int,Int) -> Bool
-selector1 [(A,B,C,D,E,F)] =
-    A + B + C + D + E + F == 5 * (A + F)
-    && D > E && E > F 
-    && isperfSqr (number [A,B,C,D,E,F]) 
+selector1 (a,b,c,d,e,f) =
+    a + b + c + d + e + f == 5 * (a + f)
+    && d > e && e > f 
+    && isperfSqr (number [a,b,c,d,e,f]) 
+    
+-- ### Main ### ---
+main :: IO()
+main = print (head (filter selector1 generator1))
