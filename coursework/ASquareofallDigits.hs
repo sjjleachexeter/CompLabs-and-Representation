296c8afdbe2d39189462fef3792b9f2bd23d0011
sjjleachexeter <sl1169@exeter.ac.uk>
2025-11-02 20:30:09 +0000

    swuareofalldigits.hs starter

--- coursework/ASquareofallDigits.hs
+++ coursework/ASquareofallDigits.hs
@@ -1,20 +1,39 @@
-prime :: Int -> Bool
-prime n
-  = factors n == [1,n]
-    where
-    factors n
-      = [f | f <- [1..n], n `mod` f == 0]
-
+import Data.List (subsequences)
+-- ### 4.1 Coprime ### --
 coprime :: Int -> Int -> Bool
 coprime a b = 
-    prime a && prime
+    gcd a b == 1
 
-squareList :: [Int] -> [Int]
-squareList (x:xs) =
-    x = x^2 
-    squareList xs
+-- ### a helper fuction to go through a list and square a function ### --
+squarerootList :: [Int] -> [Int]
+squarerootList xs = map (sqrt) xs
 
+-- ### a helper function for generator4 ### --
+number :: [Int] -> Int
+number xs
+  = totalize (reverse xs)
+    where
+    totalize []
+      = 0
+    totalize (x:xs)
+      = x + 10 * totalize xs
+
+-- ### a helper to specify the wanted digits ### --
+wantedlist :: [Int]
+wantedlist = [0..9]
+
+-- ### 4.2 generator4 ### --
 generator4 :: [Int]
-generator4 = [a = [1..100]
-            |
-]
+generator4 =
+  [ a
+  | let b = subsequences wantedlist
+  , let c = map (number) b 
+  , a <- c]
+
+-- -- ### 4.3 selector4 ### --
+-- selector4 :: 
+
+-- ### main ### --
+main :: IO()
+main = 
+    print (head (generator4))
